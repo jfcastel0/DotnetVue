b8fae907749f4f25a9fb7371167abe0d392d33f5
jfcastel0 <jcas206@wgu.edu>
2022-05-17 01:17:24 -0400

    copy/pasting this guy's crap

---
+++ src/core/Travel.Application/Common/Interfaces/IEmailService.cs
@@ -0,0 +1,8 @@
+using System.Threading.Tasks;
+using Travel.Application.Dtos.Email;
+
+namespace Travel.Application.Common.Interfaces {
+    public interface IEmailService {
+        Task SendAsync(EmailDto emailRequest);
+    }
+}
---
+++ src/core/Travel.Application/TourLists/Queries/ExportTours/ExportToursVm.cs
@@ -0,0 +1,7 @@
+namespace Travel.Application.TourLists.Queries.ExportTours {
+    public class ExportToursVm {
+        public string FileName { get; set; }
+        public string ContentType { get; set; }
+        public byte[] Content { get; set; }
+    }
+}
